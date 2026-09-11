1c057914a01ba8fe5e4cf1ae7ee4e5da9f921ca5
ceasar-yan <eduave.ceasarryan@jhcsc.edu.ph>
2026-09-11 23:36:38 +0800

    feat: TUI/design is done

--- main.sh
+++ main.sh
@@ -1,19 +1,57 @@
 #!/bin/bash
 
 # Colors
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
+GREEN=$'\e[0;32m'
+BLUE='\e[34m'
+RED='\e[31m'
 NC='\033[0m' # No Color (Reset)
 
-# Start Isolation
-tput smcup && clear; 
+cursorH() { # H = Hide
+  tput civis
+}
+cursorR() { # R = Reveal
+  tput cnorm
+}
 
-# echo -e "${BLUE}$(figlet -f slant 'Prompt to Docx')${NC}"; # Slant duh
-echo -e "${BLUE}$(figlet 'Prompt to Docx')${NC}"; # Plain but cool
+toDoc() { # convert gemini's respond to docx  file
+  local content=$1
+  local fileName=$2
+
+  echo -e "\n${BLUE}'${fileName}.docx' ${GREEN}was created successfully${NC}"
+}
+
+
+# Start Isolation
+tput smcup
+clear
 
 while true; do
-  sleep 1;
+  # echo -e "${BLUE}$(figlet -f slant 'Prompt to Docx')${NC}"; # Slant duh
+  echo -e "${BLUE}$(figlet 'Prompt to Docx')${NC}" # Plain but cool
+  echo -e "${RED}enter 'exit' to quit the script${NC}\n"
+
+  # Redeclaring Colors for read command
+  GREEN=$'\e[0;32m'
+  # BLUE=$'\e[0;34m'
+  NC=$'\e[0m'
+
+  read -e -p "${GREEN}Enter Prompt: ${NC}" prompt;
+  if [[ "${prompt}" == "exit" ]]; then
+    break
+  fi
+  read -e -p "${GREEN}Enter File Name: ${NC}" fileName;
+  if [[ "${fileName}" == "exit" ]]; then
+    break
+  fi
+
+  toDoc "$prompt" "$fileName"
+
+  echo -e "Press ENTER to reload..."
+  cursorH; read -s; cursorR
+
+  clear
+
 done
 
 # End Isolation
-bash; tput rmcup;
+tput rmcup
